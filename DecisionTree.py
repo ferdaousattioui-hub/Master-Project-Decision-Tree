b3a34c7c6b8a98854153c0ce7802c8e18d735daa
ferdaousattioui-hub <ferdaousattioui@gmail.com>
2026-07-23 21:06:15 +0100

    Update Google Drive file link in DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -31,7 +31,7 @@ if page == "1. Présentation (PDF)":
     st.write("Voici La Présentation Théorique de l'algorithme des Arbres de Décision.")
     
     # Lien exact dyal Drive jdid dyalk
-    file_id = "1ihbxU1zLXOpJAtfNZLXAjLU2mJjIUt0z"  
+    file_id = "146j4QhO8Xnmnd6Ky1tHmzClBIbRxEHRW/view?usp=sharing"  
     lien_drive_embed = f"https://drive.google.com/file/d/{file_id}/preview"
     
     st.components.v1.html(
